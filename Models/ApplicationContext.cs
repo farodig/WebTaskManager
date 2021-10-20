359d46345bdb45bb476e4e3946e5522f72b5a7a6
Maxim <mchistyakov@cnord.ru>
2021-10-20 21:37:22 +0300

    csbi-task-1: Задача выполнена - Добавлена страница задач - Добавлена страница задач с истекающим сроком окончания - (дополнительно) Добавлена страница с описанием - Добавлена модель и контекст бд - Добавлена инициализация данных

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTaskManager.Models
+{
+    /// <summary>
+    /// Контекст базы данных
+    /// </summary>
+    public class ApplicationContext : DbContext
+    {
+        /// <summary>
+        /// Задачи
+        /// </summary>
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        /// <summary>
+        /// Инициализация контекста
+        /// </summary>
+        /// <param name="options"></param>
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Models/TaskActivityStateEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTaskManager.Models
+{
+    /// <summary>
+    /// Статус задачи
+    /// </summary>
+    public enum TaskActivityStateEnum
+    {
+        /// <summary>
+        /// Неактивна
+        /// </summary>
+        InActive = 0,
+
+        /// <summary>
+        /// Активна
+        /// </summary>
+        Active = 1
+    }
+}
